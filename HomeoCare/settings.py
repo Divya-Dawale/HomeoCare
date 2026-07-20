083d1a2eb3bbd2e6fbbb9b1d37202766bb0cfecc
Sakshi <sakshi642008@gmail.com>
2026-07-20 19:29:11 +0530

    add

--- HomeoCare/settings.py
+++ HomeoCare/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = ['accounts',
     'doctors',
     'receptionist',
     'prescriptions',
+    'billing',
+    'notifications',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'accounts.User'
