53344d88766678fce0c9616e387c9a672336ffe5
Divya Dawale <divyadawale2009@gmail.com>
2026-08-04 06:44:37 +0530

    before changing version og python

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Notification(models.Model):
+
+    recipient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="notifications"
+    )
+
+    message = models.TextField()
+
+    is_read = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+
+    def __str__(self):
+        return self.message
